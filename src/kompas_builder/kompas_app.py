85e2e941712d09caaa191f03845b3aa9ef427665
Lolevtexa <ceba.kek@gmail.com>
2025-06-20 12:21:31 +0300

    Исправление и улучшение метода quit в классе KompasApp: добавлена обработка исключений и корректное завершение работы с COM.

--- src/kompas_builder/kompas_app.py
+++ src/kompas_builder/kompas_app.py
@@ -1,16 +1,25 @@
 import pythoncom
-from win32com.client import Dispatch
+from win32com.client import DispatchEx
 
 class KompasApp:
     def __init__(self):
         pythoncom.CoInitialize()
-        self.app5 = Dispatch("Kompas.Application.5")
+        self.app5 = DispatchEx("Kompas.Application.5")
         self.app5.Visible = False
 
     def get_param_struct(self, code):
         return self.app5.GetParamStruct(code)
 
     def quit(self):
-        self.app5.Quit()
-        del self.app5
-        pythoncom.CoUninitialize()
+        pass
+        try:
+            # Если у вас открыт документ, можно сначала закрыть его:
+            # self.app5.ActiveDocument2D.Close()
+            # а потом всё приложение:
+            self.app5.Quit()
+        except Exception:
+            pass
+        finally:
+            # Чистим ссылку и деинициализируем COM
+            del self.app5
+            pythoncom.CoUninitialize()
